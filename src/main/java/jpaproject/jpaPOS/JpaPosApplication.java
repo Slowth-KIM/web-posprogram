18adbae9d163c98afea2a2bb6ed2878804076876
Slowth-KIM <rt.slowth@gmail.com>
2021-05-30 16:55:59 +0900

    웹 포스프로그램 초기 파일 세팅

---
+++ src/main/java/jpaproject/jpaPOS/JpaPosApplication.java
@@ -0,0 +1,13 @@
+package jpaproject.jpaPOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpaproject/jpaPOS/JpaPosApplicationTests.java
@@ -0,0 +1,13 @@
+package jpaproject.jpaPOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
